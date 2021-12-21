5d61ccadf1709eea0aaa4221698f79e4388d5c11
Maximilian Fuchs <maxfuchs@protonmail.com>
2021-12-21 20:36:33 +0100

    test iteration

--- Prognosis.py
+++ Prognosis.py
@@ -37,3 +37,14 @@ lohn = Prognosis(
             turnus='monthly',
             end="01.06.2022")
 
+from datetime import date, timedelta
+
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+start_date = get_datetime("20.12.2021")
+end_date = get_datetime("20.12.2022")
+for single_date in daterange(start_date, end_date):
+    print(single_date.strftime("%d.%m.%Y"))
+
