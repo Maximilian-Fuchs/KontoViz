950645a6bdfad77b9d92e41f01bc39cbb98724c1
Maximilian Fuchs <maxfuchs@protonmail.com>
2021-12-21 19:36:14 +0100

    more attributes

--- Prognosis.py
+++ Prognosis.py
@@ -6,8 +6,13 @@ class Prognosis:
         self.description = description
         self.start = get_time(start)
         self.expires = False
+        self.repeats = False
+        if turnus:
+            self.repeats = True
+        self.end = False
         if end:
             self.expires = True
+        self.end = end
         self.amount = float(amount)
         self.category = category
         self.turnus = turnus
